005264b45c2430b32f90ed7875108c16ae54a7c3
yogeshkarande5026 <yogeshkarande5026@gmail.com>
2025-11-11 16:19:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+ import React from 'react';
+    import Game from './components/Game'; // If you moved Game to components folder
+
+    function App() {
+      return (
+        <div className="App">
+          <Game />
+        </div>
+      );
+    }
